9a6adcc73edb907b5efebc9eb7cdaa5c534b0e17
Gabriel de Oliveira  Barbosa <manobi.oliveira@gmail.com>
2018-11-13 08:04:10 -0200

    Linting

--- spec/agenda.spec.js
+++ spec/agenda.spec.js
@@ -3,6 +3,7 @@ const Agenda = require('../lib/agenda');
 
 describe('Agenda', () => {
     const agenda = new Agenda();
+
     describe('#constructor', () => {
         it('Extends node event emittter', () => {
             expect(agenda).to.have.property('emit');
@@ -11,9 +12,9 @@ describe('Agenda', () => {
     describe('#add', () => {
         it('add an event listener to a timestamp', () => {
             const event = new Date('05 October 2011 14:48 UTC');
+
             event.toISOString();
             agenda.add('');
         });
     });
 });
-
