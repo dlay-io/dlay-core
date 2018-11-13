7a244eacffe0044c9956117ae999ffe12dc15519
Gabriel de Oliveira  Barbosa <manobi.oliveira@gmail.com>
2018-11-13 07:57:31 -0200

    Add tests for agenda

---
+++ spec/agenda.spec.js
@@ -0,0 +1,19 @@
+const {expect} = require('chai');
+const Agenda = require('../lib/agenda');
+
+describe('Agenda', () => {
+    const agenda = new Agenda();
+    describe('#constructor', () => {
+        it('Extends node event emittter', () => {
+            expect(agenda).to.have.property('emit');
+        });
+    });
+    describe('#add', () => {
+        it('add an event listener to a timestamp', () => {
+            const event = new Date('05 October 2011 14:48 UTC');
+            event.toISOString();
+            agenda.add('');
+        });
+    });
+});
+
