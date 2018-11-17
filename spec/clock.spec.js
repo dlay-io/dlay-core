f99ef9b899bdfe7e41af8e31ad9dcf2a0edb10f6
Gabriel de Oliveira  Barbosa <manobi.oliveira@gmail.com>
2018-11-17 15:18:31 -0200

    specs for Clock #start and #stop

---
+++ spec/clock.spec.js
@@ -0,0 +1,44 @@
+const Clock = require('../lib/clock'),
+    {DateTime} = require('luxon'),
+    chai = require('chai'),
+    sinon = require('sinon'),
+    sinonChai = require('sinon-chai');
+
+chai.use(sinonChai);
+
+describe('Agenda', () => {
+    const {expect} = chai;
+    let clock = {};
+    
+    describe('#stop', () => {
+        it('stops the interval', () => {
+            const precision = 1000;
+
+            clock = new Clock(precision);
+            clock.start();
+            clock.stop();
+            expect(clock.interval).to.be.equal(null);
+        });
+    });
+
+
+    describe('#start', () => {
+        afterEach(() => {
+            clock.stop();
+        });
+
+        it('Starts the timer that triggers in precision intervals', (done) => {
+            const now = DateTime.local(),
+                precision = 1,
+                future = now.plus({'seconds': 3}),
+                timestamp = future.toMillis();
+
+            clock = new Clock(precision);
+            clock.on(timestamp, (res) => {
+                expect(res).to.be.equal('done');
+                done();
+            });
+            clock.start();
+        }).timeout(5000);
+    });
+});
