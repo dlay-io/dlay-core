474ee14a76034cc76f16e0f0571c94e620800b7d
Gabriel de Oliveira  Barbosa <manobi.oliveira@gmail.com>
2018-12-20 10:43:50 -0200

    repeatable rules

--- spec/context.spec.js
+++ spec/context.spec.js
@@ -37,4 +37,33 @@ describe('Context', () => {
             expect(ctx.retryable()).to.be.equal(false);
         });
     });
+
+    describe('#repeatable', () => {
+        it('Returns the date of next execution', () => {
+            const ctx = new Context({
+                date: '2018-12-20T12:14:25.864Z',
+                repetitions: 2,
+                repeat: {
+                    limit: 3,
+                    interval: {
+                        year: 2
+                    }
+                }
+            });
+            expect(ctx.repeatable()).to.be.equal('2020-12-20T12:14:25.864Z');
+        });
+        it('Returns false when have reached its recurrency cycle', () => {
+            const ctx = new Context({
+                date: '2018-12-20T12:14:25.864Z',
+                repetitions: 3,
+                repeat: {
+                    limit: 3,
+                    interval: {
+                        year: 2
+                    }
+                }
+            });
+            expect(ctx.repeatable()).to.be.equal(false);
+        });
+    });
 });
