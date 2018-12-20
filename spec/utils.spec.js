c7e43a2a48183e18b15bc79400a7243fa45192af
Gabriel de Oliveira  Barbosa <manobi.oliveira@gmail.com>
2018-12-20 10:54:03 -0200

    beter space naming for utils.delay

--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -11,17 +11,19 @@ describe('utils', () => {
             minutes: 1,
             seconds: 1
         }
-        it('Must convert to a future date', () => {
-            const date = utils.delay('2018-11-19T22:13:32.613Z', interval);
-            expect(date).to.be.equal('2019-12-20T23:14:33.613Z');
-        });
-        it('Must convert to a UTC future date', () => {
-            const date = utils.delay('2018-11-19T22:13:32.613', interval);
-            expect(date).to.be.equal('2019-12-20T23:14:33.613Z');
-        });
-        it('Must convert to a UTC future date', () => {
-            const date = utils.delay('2018-11-19T22:13:32.613', 'P1Y1M1DT1H1M1S');
-            expect(date).to.be.equal('2019-12-20T23:14:33.613Z');
+        describe('Must convert to a future date', () => {
+            it('Timezone informed', () => {
+                const date = utils.delay('2018-11-19T22:13:32.613Z', interval);
+                expect(date).to.be.equal('2019-12-20T23:14:33.613Z');
+            });
+            it('Timezone not informed', () => {
+                const date = utils.delay('2018-11-19T22:13:32.613', interval);
+                expect(date).to.be.equal('2019-12-20T23:14:33.613Z');
+            });
+            it('From ISO interval syntax', () => {
+                const date = utils.delay('2018-11-19T22:13:32.613', 'P1Y1M1DT1H1M1S');
+                expect(date).to.be.equal('2019-12-20T23:14:33.613Z');
+            }); 
         });
     });
 });
